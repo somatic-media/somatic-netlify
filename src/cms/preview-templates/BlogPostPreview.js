29db7304e11253e94b5175141cf1ce60d988bae7
Jay W <jay@somatic.media>
2018-06-01 01:00:30 +1000

    push

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -8,7 +8,7 @@ const BlogPostPreview = ({ entry, widgetFor }) => (
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
-    image={entry.getIn({'data', 'image'})}
+    image={entry.getIn(['data,', 'image'])}
   />
 )
 
