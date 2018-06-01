7076c25a01cc2bc952c56ada89f81aa8873963bb
Jay Whitelock <jay@somatic.media>
2018-06-01 15:21:20 +1000

    Please work

--- src/components/Blogsidebar.js
+++ src/components/Blogsidebar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid'
 
-const BlogSidebar = ({ PostContent }) => (
+const BlogSidebar = () => (
   <div>
   <h1>Test!</h1>
   </div>
